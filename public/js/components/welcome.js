f99718208ef1cb5fce1b5af9013ed9ae48ae24bf
AC287 <arthurchen287@gmail.com>
2016-06-27 19:32:17 -0700

    socialnetworking link added.

--- public/js/components/welcome.js
+++ public/js/components/welcome.js
@@ -9,6 +9,11 @@ export default React.createClass({
         <div className="profileimg">
           <img className="img-circle" src="./../assets/ArthurPI.jpg"/>
         </div>
+        <div className="social-networking">
+          <a href="https://github.com/AC287"><img src="./../assets/GitHub.png"></img></a>
+          <a href="https://www.linkedin.com/in/ac287"><img src="./../assets/In.png"></img></a>
+          <a href="https://about.me/arthurchen287"><img src="./../assets/me_3.png"></img></a>
+        </div>
       </div>
     )
   }
